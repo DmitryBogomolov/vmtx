7fcb5ce1fd63f9766867012b306d6a95a15a3c38
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2017-08-12 00:12:59 +0300

    Replace ava with mocha

--- test/index.test.js
+++ test/index.test.js
@@ -1,61 +1,64 @@
-const test = require('ava');
+const { expect } = require('chai');
 const lib = require('../src/index');
 
-test('execute simple code', (t) => {
-    const result = lib('module.exports = 10;');
+describe('Lib', () => {
+    it('executes simple code', () => {
+        const result = lib('module.exports = 10;');
 
-    t.is(result, 10);
-});
-
-test('require package', (t) => {
-    const obj = { tag: 'test-package' };
-    const result = lib('module.exports = require("test-package");', {
-        packages: {
-            'test-package': obj
-        }
+        expect(result).to.equal(10);
     });
 
-    t.is(result, obj);
-});
+    it('loads package', () => {
+        const obj = { tag: 'test-package' };
+        const result = lib('module.exports = require("test-package");', {
+            packages: {
+                'test-package': obj
+            }
+        });
 
-test('require local .js file', (t) => {
-    const result = lib('module.exports = require("./test/data/tester-1.js");');
+        expect(result).to.deep.equal(obj);
+    });
 
-    t.is(result, 102);
-});
+    it('loads local .js file', () => {
+        const result = lib('module.exports = require("./test/data/tester-1.js");');
 
-test('require local .json file', (t) => {
-    const result = lib('module.exports = require("./test/data/tester-2.json");');
+        expect(result).to.equal(102);
+    });
 
-    t.deepEqual(result, {  tag: 'test-data' });
-});
+    it('loads local .json file', () => {
+        const result = lib('module.exports = require("./test/data/tester-2.json");');
 
-test('fail when local file is not found', (t) => {
-    try {
-        lib('require("./test/data/no-file.js");');
-        t.fail('should fail');
-    } catch (e) {
-        t.pass();
-    }
-});
+        expect(result).to.deep.equal({  tag: 'test-data' });
+    });
 
-test('guess local .js file extension', (t) => {
-    const result = lib('module.exports = require("./test/data/tester-1");');
+    it('fails when local file is not found', () => {
+        let isThrown = false;
+        try {
+            lib('require("./test/data/no-file.js");');
+        } catch (_) {
+            isThrown = true;
+        }
+        expect(isThrown).to.be.true;
+    });
 
-    t.is(result, 102);
-});
+    it('guesses local .js file extension', () => {
+        const result = lib('module.exports = require("./test/data/tester-1");');
 
-test('guess local .json file extension', (t) => {
-    const result = lib('module.exports = require("./test/data/tester-2");');
+        expect(result).to.equal(102);
+    });
 
-    t.deepEqual(result, {  tag: 'test-data' });
-});
+    it('guesses local .json file extension', () => {
+        const result = lib('module.exports = require("./test/data/tester-2");');
 
-test('require complex local file', (t) => {
-    const result = lib('module.exports = require("./test/data/tester-3");');
+        expect(result).to.deep.equal({  tag: 'test-data' });
+    });
 
-    t.deepEqual(result, {
-        'tester-1': 102,
-        'tester-2': { tag: 'test-data' }
+    it('loads complex local file', () => {
+        const result = lib('module.exports = require("./test/data/tester-3");');
+
+        expect(result).to.deep.equal({
+            'tester-1': 102,
+            'tester-2': { tag: 'test-data' }
+        });
     });
 });
