b7bcd4977086ffb28d06b06da23677f3bce94dac
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2017-08-15 22:40:44 +0300

    Reorganize tests into groups

--- test/index.test.js
+++ test/index.test.js
@@ -2,114 +2,119 @@ const { expect } = require('chai');
 const lib = require('../src/index');
 
 describe('Lib', () => {
-    it('returns module.exports', () => {
-        const result = lib('module.exports = 10;');
+    describe('exports', () => {
+        it('returns module.exports', () => {
+            const result = lib('module.exports = 10;');
 
-        expect(result).to.equal(10);
-    });
+            expect(result).to.equal(10);
+        });
 
-    it('returns exports', () => {
-        const result = lib(`
+        it('returns exports', () => {
+            const result = lib(`
             exports.a = 1;
             exports.b = 2;
         `);
 
-        expect(result).to.deep.equal({
-            a: 1,
-            b: 2
+            expect(result).to.deep.equal({
+                a: 1,
+                b: 2
+            });
         });
     });
 
-    it('loads package', () => {
-        const obj = { tag: 'test-package' };
-        const result = lib({
-            code: 'module.exports = require("test-package");',
-            packages: {
-                'test-package': obj
+    describe('loading packages', () => {
+        it('loads package', () => {
+            const obj = { tag: 'test-package' };
+            const result = lib({
+                code: 'module.exports = require("test-package");',
+                packages: {
+                    'test-package': obj
+                }
+            });
+
+            expect(result).to.equal(obj);
+        });
+
+        it('loads local .js file', () => {
+            const result = lib('module.exports = require("./test/data/tester-1.js");');
+
+            expect(result).to.equal(102);
+        });
+
+        it('loads local .json file', () => {
+            const result = lib('module.exports = require("./test/data/tester-2.json");');
+
+            expect(result).to.deep.equal({ tag: 'test-data' });
+        });
+
+        it('fails when local file is not found', () => {
+            let isThrown = false;
+            try {
+                lib('require("./test/data/no-file.js");');
+            } catch (_) {
+                isThrown = true;
             }
+            expect(isThrown).to.be.true;
         });
 
-        expect(result).to.equal(obj);
-    });
+        it('guesses .js file extension', () => {
+            const result = lib('module.exports = require("./test/data/tester-1");');
 
-    it('loads local .js file', () => {
-        const result = lib('module.exports = require("./test/data/tester-1.js");');
+            expect(result).to.equal(102);
+        });
 
-        expect(result).to.equal(102);
-    });
+        it('guesses .json file extension', () => {
+            const result = lib('module.exports = require("./test/data/tester-2");');
 
-    it('loads local .json file', () => {
-        const result = lib('module.exports = require("./test/data/tester-2.json");');
+            expect(result).to.deep.equal({ tag: 'test-data' });
+        });
 
-        expect(result).to.deep.equal({ tag: 'test-data' });
-    });
+        it('loads complex file', () => {
+            const result = lib('module.exports = require("./test/data/tester-3");');
 
-    it('fails when local file is not found', () => {
-        let isThrown = false;
-        try {
-            lib('require("./test/data/no-file.js");');
-        } catch (_) {
-            isThrown = true;
-        }
-        expect(isThrown).to.be.true;
-    });
-
-    it('guesses .js file extension', () => {
-        const result = lib('module.exports = require("./test/data/tester-1");');
-
-        expect(result).to.equal(102);
-    });
-
-    it('guesses .json file extension', () => {
-        const result = lib('module.exports = require("./test/data/tester-2");');
-
-        expect(result).to.deep.equal({ tag: 'test-data' });
-    });
-
-    it('loads complex file', () => {
-        const result = lib('module.exports = require("./test/data/tester-3");');
-
-        expect(result).to.deep.equal({
-            'tester-1': 102,
-            'tester-2': { tag: 'test-data' }
+            expect(result).to.deep.equal({
+                'tester-1': 102,
+                'tester-2': { tag: 'test-data' }
+            });
         });
     });
 
-    it('caches loaded files', () => {
-        const result = lib(`
+    describe('cache', () => {
+        it('caches loaded files', () => {
+            const result = lib(`
             const obj = require('./test/data/tester-2');
             require('./test/data/tester-4');
             module.exports = obj;
         `);
 
-        expect(result).to.deep.equal({
-            tag: 'test-data',
-            'test-change': 2
+            expect(result).to.deep.equal({
+                tag: 'test-data',
+                'test-change': 2
+            });
         });
-    });
 
-    it('caches loaded modules', () => {
-        const code = `
+        it('caches loaded modules', () => {
+            const code = `
             const obj1 = require('test-package');
             const obj2 = require('test-package');
             obj2['test-change'] = 2;
             module.exports = obj1;
         `;
-        const result = lib({
-            code,
-            packages: {
-                'test-package': { tag: 'test-package' }
-            }
+            const result = lib({
+                code,
+                packages: {
+                    'test-package': { tag: 'test-package' }
+                }
+            });
+
+            expect(result).to.deep.equal({
+                tag: 'test-package',
+                'test-change': 2
+            });
         });
 
-        expect(result).to.deep.equal({
-            tag: 'test-package',
-            'test-change': 2
-        });
-    });
-
-    it('caches failed files', () => {
-        const result = lib(`
+        it('caches failed files', () => {
+            const result = lib(`
             try {
                 require('./test/no-file');
             } catch (e) {
@@ -122,69 +127,76 @@ describe('Lib', () => {
             }
         `);
 
-        expect(result.e1).to.equal(result.e2);
-    });
-
-    it('executes file instead of code', () => {
-        const result = lib({
-            file: './test/data/tester-5'
-        });
-
-        expect(result).to.deep.equal({
-            a: 103,
-            b: {
-                tag: 'test-data',
-                test: 'Hello'
-            }
+            expect(result.e1).to.equal(result.e2);
         });
     });
 
-    it('allows to change root dir', () => {
-        const result = lib({
-            code: 'module.exports = require("./tester-2");',
-            root: './test/data'
-        });
+    describe('file execution', () => {
+        it('executes file instead of code', () => {
+            const result = lib({
+                file: './test/data/tester-5'
+            });
 
-        expect(result).to.deep.equal({
-            tag: 'test-data'
-        });
-    });
-
-    it('allows to define *console*', () => {
-        let arg = null;
-        lib({
-            code: 'console.log("Hello");',
-            globals: {
-                console: {
-                    log: (message) => { arg = message; }
+            expect(result).to.deep.equal({
+                a: 103,
+                b: {
+                    tag: 'test-data',
+                    test: 'Hello'
                 }
-            }
+            });
         });
-
-        expect(arg).to.equal('Hello');
     });
 
-    it('allows to define custom globals', () => {
-        const result = lib({
-            code: 'exports.a = a; exports.b = b;',
-            globals: {
-                a: 1, b: 2
-            }
-        });
+    describe('root dir', () => {
+        it('allows to change root dir', () => {
+            const result = lib({
+                code: 'module.exports = require("./tester-2");',
+                root: './test/data'
+            });
 
-        expect(result).to.deep.equal({ a: 1, b: 2 });
+            expect(result).to.deep.equal({
+                tag: 'test-data'
+            });
+        });
     });
 
-    it('does not allow to redefine *module*, *exports* and *require*', () => {
-        const result = lib({
-            code: 'module.exports = require("./test/data/tester-1");',
-            globals: {
-                module: 10,
-                exports: 'test',
-                require: () => { }
-            }
+    describe('globals', () => {
+        it('allows to define *console*', () => {
+            let arg = null;
+            lib({
+                code: 'console.log("Hello");',
+                globals: {
+                    console: {
+                        log: (message) => { arg = message; }
+                    }
+                }
+            });
+
+            expect(arg).to.equal('Hello');
         });
 
-        expect(result).to.equal(102);
+        it('allows to define custom globals', () => {
+            const result = lib({
+                code: 'exports.a = a; exports.b = b;',
+                globals: {
+                    a: 1, b: 2
+                }
+            });
+
+            expect(result).to.deep.equal({ a: 1, b: 2 });
+        });
+
+        it('does not allow to redefine *module*, *exports* and *require*', () => {
+            const result = lib({
+                code: 'module.exports = require("./test/data/tester-1");',
+                globals: {
+                    module: 10,
+                    exports: 'test',
+                    require: () => { }
+                }
+            });
+
+            expect(result).to.equal(102);
+        });
     });
 });
